36e2b8343c2c1610c7bce9cfbe6ecd17ab37cd53
unknown <waldyiskandar@gmail.com>
2023-09-01 22:13:11 +0700

    Section 9 API part 2

---
+++ API part 2/validate.headers.cy.js
@@ -0,0 +1,15 @@
+describe('Validate API', () => {
+    it('Success validate header content-type', () => {
+        cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon')
+
+        cy.get('@pokemon').its('headers').its('content-type')
+        .should('include', 'application/json; charset=utf-8')
+        
+        cy.get('@pokemon').should((response) => {
+            expect(response.body).to.have.property('base_experience')
+            // expect(response.body.abilities[0].ability.name).to.eq('limber')
+        });
+    });
+
+    
+});
