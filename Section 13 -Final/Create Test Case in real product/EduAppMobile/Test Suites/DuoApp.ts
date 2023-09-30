ef8c48a08057c76f7a7d3c6df3a5f72638be44e4
unknown <waldyiskandar@gmail.com>
2023-09-30 16:37:29 +0700

    Section 13 Final

---
+++ Section 13 -Final/Create Test Case in real product/EduAppMobile/Test Suites/DuoApp.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DuoApp</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>afd8b0c7-d3d5-449c-aaa5-290ceb3c4e91</testSuiteGuid>
+   <testCaseLink>
+      <guid>887c78b0-d21a-48e3-9ba4-17e3f943ef85</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/OpenDetail</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fbae6e60-e8fd-469b-8406-55902bfc8d3d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Calculator/ChangeTheme</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
